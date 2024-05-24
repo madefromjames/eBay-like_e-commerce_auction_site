da11154ae279a4e35656605a517f14b8806525f0
madefromjames <madefromjames@gmail.com>
2024-05-24 15:36:03 +0100

    test configuration

--- commerce/settings.py
+++ commerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 
-CSRF_COOKIE_SECURE = True  # Set to True if served over HTTPS, otherwise False
+# CSRF_COOKIE_SECURE = True  # Set to True if served over HTTPS, otherwise False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
